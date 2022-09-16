8a5ae339be4914a607afbca392a75c1fcd0cb7fb
nclhowell <nclhowell@gmail.com>
2022-09-15 19:23:16 -0700

    commit 2

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+/* app.get("/", (req, res) => {
+    res.send("Totally believe this worked!")
+})
+*/
+app.use("/", require("./routes"))
+
+app.listen(port, () => {
+    console.log(`Testing App listening on port ${port}`)
+})
