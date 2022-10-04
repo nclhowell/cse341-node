c30e1826041d31ae14b5d6c7c2c3a5ad19a77872
nclhowell <nclhowell@gmail.com>
2022-10-03 19:59:57 -0700

    Week 3

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,10 +1,10 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db("cse341").collection('contacts').find();
+  const result = await mongodb.getDb().db("cse341").collection("contacts").find();
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
 };
@@ -14,12 +14,71 @@ const getSingle = async (req, res, next) => {
   const result = await mongodb
     .getDb()
     .db("cse341")
-    .collection('contacts')
-    .find({ _id: userId });
+    .collection("contacts")
+    .find({
+      _id: userId
+    });
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists[0]);
   });
 };
 
-module.exports = { getAll, getSingle };
+const createSingle = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  console.log(contact)
+
+  const response = await mongodb.getDb().db("cse341").collection("contacts").insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  }
+};
+
+const updateSingle = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db("cse341")
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount = 1) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Catchall Error Occurred.  Could have been anything!');
+  }
+};
+
+const deleteSingle = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db("cse341").collection('contacts').deleteOne({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  createSingle,
+  updateSingle,
+  deleteSingle
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
+router.post('/', contactsController.createSingle);
+
+router.put('/:id', contactsController.updateSingle);
+
+router.delete('/:id', contactsController.deleteSingle);
+
 module.exports = router;
